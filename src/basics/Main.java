84ebacfb806876beeae28620cd282142f0712e43
rafalzbanski <92453576+rafalzbanski@users.noreply.github.com>
2024-04-23 23:46:35 +0200

    .

--- src/basics/Main.java
+++ src/basics/Main.java
@@ -1,5 +1,7 @@
 package basics;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] Argass) {
         // single line comment
@@ -13,6 +15,21 @@ public class Main {
          */
 
         System.out.println("Hello World!"); // printing "Hello World!" on screen
+
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+
+        int reverser = 0;
+
+        while (i != 0) {
+            int digit = i % 10;
+            if (digit != 0) {
+                reverser = reverser * 10 + digit;
+            }
+            i /= 10;
+        }
+        System.out.println(reverser);
+
 
     }
 }
